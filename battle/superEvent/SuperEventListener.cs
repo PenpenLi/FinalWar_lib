4dc10f7ade5eedf0244d2f8f2836ce0e46b9bee0
Land <ludidilu@163.com>
2016-09-15 04:06:04 +0800

    修改SuperEvent的命名空间

--- battle/superEvent/SuperEventListener.cs
+++ battle/superEvent/SuperEventListener.cs
@@ -1,153 +1,157 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
-class SuperEventListenerUnit
+namespace superEvent
 {
-    public int index;
-    public string eventName;
-    public Action<SuperEvent> callBack;
-
-    public SuperEventListenerUnit(int _index,string _eventName,Action<SuperEvent> _callBack)
+    internal class SuperEvent
     {
-        index = _index;
-        eventName = _eventName;
-        callBack = _callBack;
-    }
-}
+        internal string eventName;
+        internal int index;
+        internal object[] datas;
 
-class SuperEvent
-{
-    public string eventName;
-    public int index;
-    public object[] datas;
-
-    public SuperEvent(string _eventName)
-    {
-        eventName = _eventName;
-    }
-
-    public SuperEvent(string _eventName,params object[] _objs)
-    {
-        eventName = _eventName;
-        datas = _objs;
-    }
-
-    public SuperEvent(string _eventName,int _index,object[] _datas)
-    {
-        eventName = _eventName;
-        index = _index;
-        datas = _datas;
-    }
-}
-
-class SuperEventListener
-{
-    private Dictionary<int, SuperEventListenerUnit> dicWithID = new Dictionary<int, SuperEventListenerUnit>();
-    private Dictionary<string, Dictionary<Action<SuperEvent>, SuperEventListenerUnit>> dicWithEvent = new Dictionary<string, Dictionary<Action<SuperEvent>, SuperEventListenerUnit>>();
-
-    private int nowIndex;
-
-    public int AddListener(string _eventName,Action<SuperEvent> _callBack)
-    {
-        SuperEventListenerUnit unit = new SuperEventListenerUnit(nowIndex, _eventName, _callBack);
-
-        nowIndex++;
-
-        dicWithID.Add(unit.index, unit);
-
-        Dictionary<Action<SuperEvent>, SuperEventListenerUnit> dic;
-
-        if (dicWithEvent.ContainsKey(_eventName))
+        internal SuperEvent(string _eventName)
         {
-            dic = dicWithEvent[_eventName];
-        }
-        else
-        {
-            dic = new Dictionary<Action<SuperEvent>, SuperEventListenerUnit>();
-
-            dicWithEvent.Add(_eventName, dic);
+            eventName = _eventName;
         }
 
-        dic.Add(_callBack, unit);
+        internal SuperEvent(string _eventName, params object[] _objs)
+        {
+            eventName = _eventName;
+            datas = _objs;
+        }
 
-        return unit.index;
+        internal SuperEvent(string _eventName, int _index, object[] _datas)
+        {
+            eventName = _eventName;
+            index = _index;
+            datas = _datas;
+        }
     }
 
-    public void RemoveListener(int _index)
+    internal class SuperEventListener
     {
-        if (dicWithID.ContainsKey(_index))
+        private class SuperEventListenerUnit
         {
-            SuperEventListenerUnit unit = dicWithID[_index];
+            internal int index;
+            internal string eventName;
+            internal Action<SuperEvent> callBack;
 
-            dicWithID.Remove(_index);
-
-            Dictionary<Action<SuperEvent>, SuperEventListenerUnit> dic = dicWithEvent[unit.eventName];
-
-            dic.Remove(unit.callBack);
-
-            if (dic.Count == 0)
+            internal SuperEventListenerUnit(int _index, string _eventName, Action<SuperEvent> _callBack)
             {
-                dicWithEvent.Remove(unit.eventName);
+                index = _index;
+                eventName = _eventName;
+                callBack = _callBack;
             }
         }
-    }
 
-    public void RemoveListener(string _eventName, Action<SuperEvent> _callBack)
-    {
-        if (dicWithEvent.ContainsKey(_eventName))
+        private Dictionary<int, SuperEventListenerUnit> dicWithID = new Dictionary<int, SuperEventListenerUnit>();
+        private Dictionary<string, Dictionary<Action<SuperEvent>, SuperEventListenerUnit>> dicWithEvent = new Dictionary<string, Dictionary<Action<SuperEvent>, SuperEventListenerUnit>>();
+
+        private int nowIndex;
+
+        internal int AddListener(string _eventName, Action<SuperEvent> _callBack)
         {
-            Dictionary<Action<SuperEvent>, SuperEventListenerUnit> dic = dicWithEvent[_eventName];
+            SuperEventListenerUnit unit = new SuperEventListenerUnit(nowIndex, _eventName, _callBack);
 
-            if (dic.ContainsKey(_callBack))
+            nowIndex++;
+
+            dicWithID.Add(unit.index, unit);
+
+            Dictionary<Action<SuperEvent>, SuperEventListenerUnit> dic;
+
+            if (dicWithEvent.ContainsKey(_eventName))
             {
-                SuperEventListenerUnit unit = dic[_callBack];
+                dic = dicWithEvent[_eventName];
+            }
+            else
+            {
+                dic = new Dictionary<Action<SuperEvent>, SuperEventListenerUnit>();
 
-                dicWithID.Remove(unit.index);
+                dicWithEvent.Add(_eventName, dic);
+            }
 
-                dic.Remove(_callBack);
+            dic.Add(_callBack, unit);
+
+            return unit.index;
+        }
+
+        internal void RemoveListener(int _index)
+        {
+            if (dicWithID.ContainsKey(_index))
+            {
+                SuperEventListenerUnit unit = dicWithID[_index];
+
+                dicWithID.Remove(_index);
+
+                Dictionary<Action<SuperEvent>, SuperEventListenerUnit> dic = dicWithEvent[unit.eventName];
+
+                dic.Remove(unit.callBack);
 
                 if (dic.Count == 0)
                 {
-                    dicWithEvent.Remove(_eventName);
+                    dicWithEvent.Remove(unit.eventName);
+                }
+            }
+        }
+
+        internal void RemoveListener(string _eventName, Action<SuperEvent> _callBack)
+        {
+            if (dicWithEvent.ContainsKey(_eventName))
+            {
+                Dictionary<Action<SuperEvent>, SuperEventListenerUnit> dic = dicWithEvent[_eventName];
+
+                if (dic.ContainsKey(_callBack))
+                {
+                    SuperEventListenerUnit unit = dic[_callBack];
+
+                    dicWithID.Remove(unit.index);
+
+                    dic.Remove(_callBack);
+
+                    if (dic.Count == 0)
+                    {
+                        dicWithEvent.Remove(_eventName);
+                    }
+                }
+            }
+        }
+
+        internal void DispatchEvent(SuperEvent e)
+        {
+            if (dicWithEvent.ContainsKey(e.eventName))
+            {
+                Dictionary<Action<SuperEvent>, SuperEventListenerUnit> dic = dicWithEvent[e.eventName];
+
+                Action[] arr = new Action[dic.Count];
+
+                Dictionary<Action<SuperEvent>, SuperEventListenerUnit>.Enumerator enumerator = dic.GetEnumerator();
+
+                int i = 0;
+
+                while (enumerator.MoveNext())
+                {
+                    KeyValuePair<Action<SuperEvent>, SuperEventListenerUnit> pair = enumerator.Current;
+
+                    SuperEvent ev = new SuperEvent(e.eventName, pair.Value.index, e.datas);
+
+                    Action del = delegate ()
+                    {
+                        pair.Key(ev);
+                    };
+
+                    arr[i] = del;
+
+                    i++;
+                }
+
+                for (i = 0; i < arr.Length; i++)
+                {
+                    arr[i]();
                 }
             }
         }
     }
-
-    public void DispatchEvent(SuperEvent e)
-    {
-        if (dicWithEvent.ContainsKey(e.eventName))
-        {
-            Dictionary<Action<SuperEvent>, SuperEventListenerUnit> dic = dicWithEvent[e.eventName];
-
-            Action[] arr = new Action[dic.Count];
-
-            Dictionary<Action<SuperEvent>, SuperEventListenerUnit>.Enumerator enumerator = dic.GetEnumerator();
-
-            int i = 0;
-
-            while (enumerator.MoveNext())
-            {
-                KeyValuePair<Action<SuperEvent>, SuperEventListenerUnit> pair = enumerator.Current;
-
-                SuperEvent ev = new SuperEvent(e.eventName, pair.Value.index, e.datas);
-
-                Action del = delegate ()
-                {
-                    pair.Key(ev);
-                };
-
-                arr[i] = del;
-
-                i++;
-            }
-
-            for(i = 0; i < arr.Length; i++)
-            {
-                arr[i]();
-            }
-        }
-    }
 }
+
+
 
