846fd52164df9139d1d8cd42e4cd003ce4c12500
Land <ludidilu@163.com>
2018-04-18 00:40:45 +0800

    a

--- battle/battleCore/BattleConst.cs
+++ battle/battleCore/BattleConst.cs
@@ -15,7 +15,7 @@
         public const int SPEED_GAP_0 = 1;
         public const int SPEED_GAP_1 = 2;
 
-        public const int MAX_FEAR_VALUE = 4;
+        public const int MAX_FEAR_VALUE = 3;
 
         public const string FIX_ATTACK_SHIELD_TO_DAMAGE = "fixAttackShieldToDamage";
         public const string FIX_BE_ATTACKED_SHIELD_TO_DAMAGE = "fixBeAttackedShieldToDamage";
