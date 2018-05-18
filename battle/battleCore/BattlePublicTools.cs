1aa569691093be7c9be68dc6411ac01fd92afff9
ludidilu <ludidilu@163.com>
2018-05-18 17:03:05 +0800

    a

--- battle/battleCore/BattlePublicTools.cs
+++ battle/battleCore/BattlePublicTools.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace FinalWar
 {
-    public class BattlePublicTools
+    public static class BattlePublicTools
     {
         public static List<int> GetNeighbourPos(MapData _mapData, int _pos)
         {
